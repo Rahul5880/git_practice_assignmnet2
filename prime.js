311a961714f3e82f42f76423b3e60904a7985899
Rahul5880 <kesharirahul2003@gmail.com>
2022-12-08 17:43:24 +0530

    code fixed prime

--- prime.js
+++ prime.js
@@ -1,10 +1,20 @@
 function checkprime(num){
- if(num===2){
+ let count = 0;
+
+  for(let i=0;i<num;i++){
+    if(num%i==0){
+      count++;
+      
+  }
+}
+if(count == 2){
+return true; 
+}
+return false;
+}
+ let ans= checkprime(13);
+ if(ans===true){
         console.log("prime");
     }else{
         console.log("Not prime");
     }
-}
-
-checkprime(2)
-checkprime(5)
